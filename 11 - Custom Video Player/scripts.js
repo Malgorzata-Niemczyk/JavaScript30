41792d4565f91a2cd3889fa459092a3d06e40dcb
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-04 22:33:32 +0100

    grab the buttons and inputs elements using querySelector() method

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,4 @@
+const playerBtn = document.querySelector('.player__button');
+const sliderInput = document.querySelectorAll('.player__slider');
+const skipBtns = document.querySelectorAll('.player__button');
+
