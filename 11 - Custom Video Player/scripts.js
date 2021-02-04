0aad24dfafc0a4107773db033c5e3938d24c0525
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-04 23:03:03 +0100

    create the togglePlay function and add a click event on the playerBtn element to trigger this function

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -6,3 +6,19 @@ const playerBtn = document.querySelector('.player__button');
 const sliderInputs = document.querySelectorAll('.player__slider');
 const skipBtns = document.querySelectorAll('[data-skip]');
 
+// to play and stop the video
+function togglePlay() {
+    switch (true) {
+        case video.paused:
+            video.play();
+            break;
+        default:
+            video.pause()
+    }
+}
+
+// event listeners
+playerBtn.addEventListener('click', togglePlay);
+
+
+
