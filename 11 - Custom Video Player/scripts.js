762e643a9d2f1796f471749fcf559178e07cb315
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-02-04 22:50:55 +0100

    grab the rest of the elements: video, player, progressWrapper and progressBar

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,4 +1,8 @@
+const player = document.querySelector('.player');
+const video = document.querySelector('.viewer');
+const progressWrapper = document.querySelector('.progress');
+const progressBar = document.querySelector('.progress_filled');
 const playerBtn = document.querySelector('.player__button');
-const sliderInput = document.querySelectorAll('.player__slider');
-const skipBtns = document.querySelectorAll('.player__button');
+const sliderInputs = document.querySelectorAll('.player__slider');
+const skipBtns = document.querySelectorAll('[data-skip]');
 
